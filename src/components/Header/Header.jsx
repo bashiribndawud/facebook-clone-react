c235334bbb9b64b235b7caff2a8942bf6290c797
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2023-02-20 22:37:27 +0100

    setup header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
+import SwitchVideoIcon from "@material-ui/icons/SwitchVideo";
+import GroupIcon from "@material-ui/icons/Group";
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import Avatar from "@material-ui/core/Avatar";
+import './Header.css';
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+          alt="Logo"
+        />
+        <div className="search">
+          <SearchIcon fontSize="large" className="search-icon" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+      <div className="header__center">
+        <div className="icon__option active">
+          <IconButton>
+            <HomeIcon />
+          </IconButton>
+        </div>
+        <div className="icon__option">
+          <IconButton>
+            <SwitchVideoIcon />
+          </IconButton>
+        </div>
+        <div className="icon__option">
+          <IconButton>
+            <GroupIcon />
+          </IconButton>
+        </div>
+        <div className="icon__option">
+          <IconButton>
+            <VideogameAssetIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <IconButton>
+          <ChatIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <Avatar alt="Cindy Baker" src="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnQeEZQfpXcEUe-5k7CFwhcBMRbeekzzA",
+  authDomain: "facebook-clone-61eb6.firebaseapp.com",
+  projectId: "facebook-clone-61eb6",
+  storageBucket: "facebook-clone-61eb6.appspot.com",
+  messagingSenderId: "648977280200",
+  appId: "1:648977280200:web:97ca542a90921ff21a49fd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
